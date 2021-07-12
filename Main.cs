a1a89ba4e78f0a14fb9eb0e664b4d9babba43f80
Akash Kashid <62595837+Akashid17@users.noreply.github.com>
2021-07-12 11:32:59 +0530

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,29 @@
+/*
+2. Program to print 5 times “Hello World” on screen.
+*/
+
+using System;
+
+class StringX
+{
+	public
+	void Display()
+	{
+		int i = 1;
+
+		do
+		{
+			Console.WriteLine("Hello World");
+			i += 1;
+		}while(i <= 5);
+	}
+}
+
+class main
+{
+	static void Main(string[] Arg)
+	{
+		StringX sobj = new StringX();
+		sobj.Display();
+	}
+}
